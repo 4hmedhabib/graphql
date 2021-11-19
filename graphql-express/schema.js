72558f78bf766464faf4663503e8ad2ed011acde
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-11-19 17:35:22 +0300

    added getAllUsers getUser createUser

--- graphql-express/schema.js
+++ graphql-express/schema.js
@@ -1,8 +1,73 @@
-const graphql, {GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString } = require('graphql');
+const userData = require('./MOCK_DATA.json');
+const graphql = require('graphql');
+const { GraphQLObjectType, GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList } = require('graphql');
 
+const UserType = new GraphQLObjectType({
+	name: 'User',
+	fields: () => ({
+		id: { type: GraphQLInt },
+		firstName: { type: GraphQLString },
+		lastName: { type: GraphQLString },
+		email: { type: GraphQLString },
+		password: { type: GraphQLString }
+	})
+});
 
-const RootQuery = 'query';
-const Mutation = 'mutation';
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		getAllUsers: {
+			type: new GraphQLList(UserType),
+			args: { id: { type: GraphQLInt }, firstName: { type: GraphQLString } },
+			resolve(parent, args) {
+				return userData;
+			}
+		},
+		getUser: {
+			type: UserType,
+			args: {
+				id: {
+					type: GraphQLInt
+				},
+				firstName: {
+					type: GraphQLString
+				},
+				lastName: {
+					type: GraphQLString
+				}
+			},
+			resolve(parent, args) {
+				return userData.find((user) => user.id === args.id);
+			}
+		}
+	}
+});
+
+const Mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		createUser: {
+			type: UserType,
+			args: {
+				firstName: { type: GraphQLString },
+				lastName: { type: GraphQLString },
+				email: { type: GraphQLString },
+				password: { type: GraphQLString }
+			},
+			resolve(parents, args) {
+				console.log(args);
+				userData.push({
+					id: userData.length + 1,
+					firstName: args.firstName,
+					lastName: args.lastName,
+					email: args.email,
+					password: args.password
+				});
+				return args;
+			}
+		}
+	}
+});
 
 const schema = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
 
